263cf08591f23d1f9a4bf4449296f2542a4e2158
Karan R Joshi <karan.rjoshi13@gmail.com>
2025-08-21 15:24:11 +0530

    Final Tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
-import Hero from "./components/hero/Hero";
-import Services from "./components/services/Services";
-import Portfolio from "./components/portfolio/Portfolio";
-import Contact from "./components/contact/Contact";
-import { ToastProvider } from './components/contact/ToastContext';
+// import Hero from "./components/hero/Hero";
+// import Services from "./components/services/Services";
+// import Portfolio from "./components/portfolio/Portfolio";
+// import Contact from "./components/contact/Contact";
+
+import { lazy, Suspense } from "react";
+import LazyLoad from "react-lazyload";
+import { ToastProvider } from "./components/contact/ToastContext";
+
+const Hero = lazy(() => import("./components/hero/Hero"));
+const Services = lazy(() => import("./components/services/Services"));
+const Portfolio = lazy(() => import("./components/portfolio/Portfolio"));
+const Contact = lazy(() => import("./components/contact/Contact"));
 
 const App = () => {
   return (
     <ToastProvider>
     <div className="container">
-      <section id="#home">
-        <Hero />
-      </section>
-      <section id="#services">
-        <Services />
-      </section>
-      {/* <section id="#portfolio"> */}
-        <Portfolio />
-      {/* </section> */}
-      <section id="#contact">
-        <Contact />
-      </section>
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"100vh"} offset={-100}>
+          <section id="#home">
+            <Hero />
+          </section>
+        </LazyLoad>
+      </Suspense>
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"100vh"} offset={-100}>
+          <section id="#services">
+            <Services />
+          </section>{" "}
+        </LazyLoad>
+      </Suspense>
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"600vh"} offset={-100}>
+          {/* <section id="#portfolio"> */}
+          <Portfolio />
+          {/* </section> */}{" "}
+        </LazyLoad>
+      </Suspense>
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"100vh"} offset={-100}>
+          <section id="#contact">
+            <Contact />
+          </section>{" "}
+        </LazyLoad>
+      </Suspense>
     </div>
     </ToastProvider>
   );
 };
 
-export default App
+export default App;
